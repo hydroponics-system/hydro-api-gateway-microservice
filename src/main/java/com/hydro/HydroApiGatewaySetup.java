fbd5d915c3fc7de982de0a6512cc6521a0bb1ca0
Samuel Butler <sambutler1017@icloud.com>
2022-08-28 19:39:16 -0400

    no message

--- src/main/java/com/hydro/HydroApiGatewaySetup.java
+++ src/main/java/com/hydro/HydroApiGatewaySetup.java
@@ -1,20 +1,15 @@
 package com.hydro;
 
-import java.security.NoSuchAlgorithmException;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
- * Empty main for building out jar file.
+ * API Gateway setup class for performing scan of filters and packages needed.
  * 
  * @author Sam Butler
  * @since August 27, 2022
  */
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @ServletComponentScan
-public class HydroApiGatewayApplication {
-
-	public static void main(String[] args) throws NoSuchAlgorithmException {}
-}
+public class HydroApiGatewaySetup {}
