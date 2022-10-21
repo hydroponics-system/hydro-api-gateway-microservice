3b96d30cced2f17cd9b4ab927b0dbdeb4ba52046
Samuel Butler <sambutler1017@icloud.com>
2022-10-21 11:00:38 -0400

    no message

--- src/test/java/com/hydro/api/gateway/validators/EndpointInboundValidatorTest.java
+++ src/test/java/com/hydro/api/gateway/validators/EndpointInboundValidatorTest.java
@@ -1,6 +1,16 @@
 package com.hydro.api.gateway.validators;
 
-import com.hydro.test.factory.annotations.HydroTest;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import com.hydro.api.gateway.validator.EndpointInboundValidator;
+import com.hydro.common.jwt.utility.JwtHolder;
+import com.hydro.test.factory.annotations.HydroServiceTest;
 
 /**
  * Test for Endpoint Inbound Validator.
@@ -8,7 +18,22 @@ import com.hydro.test.factory.annotations.HydroTest;
  * @author Sam Butler
  * @since August 25, 2022
  */
-@HydroTest
+@HydroServiceTest
 public class EndpointInboundValidatorTest {
 
+    @Mock
+    private JwtHolder jwtHolder;
+
+    @InjectMocks
+    private EndpointInboundValidator endpointInboundValidator;
+
+    @Test
+    public void testShouldNotFilterRequest() {
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.setServletPath("/api/authenticate");
+        req.setMethod("POST");
+
+        endpointInboundValidator.validateRequest(req);
+        verify(jwtHolder, never()).setToken(anyString());
+    }
 }
